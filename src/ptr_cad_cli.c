2428854016c1b441632151df4c41896435c9b0a9
Arthur Cruz <artur0208alves@gmail.com>
2024-11-14 14:47:12 -0300

    Cadastro cliente pronto e funcional

--- src/ptr_cad_cli.c
+++ src/ptr_cad_cli.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 
-void cadastro(char *nome, char *email, char *idade){
+void cadastro(char *nome, char *email, int *idade){
     FILE *arquivo;
     arquivo = fopen("files/cad_cli.txt","a");
-    fprintf(arquivo,nome);
-    fprintf(arquivo,email);
-    fprintf(arquivo,idade);
+    fprintf(arquivo,"Nome: %s\n",nome);
+    fprintf(arquivo,"E-Mail: %s\n",email);
+    fprintf(arquivo,"Idade: %d\n",*idade);
+    fprintf(arquivo,"--------------------------");
     fclose(arquivo);
 }
 int main(){
     char nome[30];
     char email[50];
-    char idade[2];
+    int idade;
 
     printf("Digite o seu nome e tecle Enter:\n");
     scanf("%[^\n]s",nome);
-
     printf("Digite o seu email e tecle Enter:\n");
     scanf("%s",email);
-
     printf("Digite o sua idade e tecle Enter:\n");
-    scanf("%s",idade);
+    scanf("%d",&idade);
+    cadastro(nome,email,&idade);
+    printf("Cadastrado com sucesso!\n ");
 
-    cadastro(nome,email,idade);
-    printf("Cadastrou?\n");
     return 0;
 }
